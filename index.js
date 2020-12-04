e7b6b76b80f1843345c01fa83baa385aec177ebd
obzenner <obzenner@Kinjas-MBP.lan>
2020-12-04 19:03:14 +0100

    day 4

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 const { day1Solution } = require('./day1/index');
 const { day2Solution } = require('./day2/index');
 const { day3Solution } = require('./day3/index');
+const { day4Solution } = require('./day4/index');
 const day1Result = day1Solution();
 const day2Result = day2Solution();
 const day3Result = day3Solution();
+const day4Result = day4Solution();
 
-console.log(day3Result)
+console.log(day4Result)
