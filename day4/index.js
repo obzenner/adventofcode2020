c8d73aaa325e00217b208dcb626a145cd3f7c3ae
obzenner <obzenner@Kinjas-MBP.lan>
2020-12-05 00:01:52 +0100

    false

--- day4/index.js
+++ day4/index.js
@@ -29,13 +29,14 @@ const getPassportFields = (input) => {
 const sortAndJoin = (array = []) => {
     return array.sort().join('');
 }
+
 const requiredFields = [
-    ['byr', /\b(19[2-8][0-9]|199[0-9]|200[0-2])\b/],
-    ['iyr', /\b(201[0-9]|2020)\b/],
-    ['eyr', /\b(202[0-9]|2030)\b/],
-    ['hgt', /\b(1[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in\b/],
-    ['hcl', /#([\da-f]{2})([\da-f]{2})([\da-f]{2})/],
-    ['ecl', /(amb|blu|brn|gry|grn|hzl|oth)/],
+    ['byr', /^19[2-9][0-9]|200[0-2]$/],
+    ['iyr', /^201[0-9]|2020$/],
+    ['eyr', /^202[0-9]|2030$/],
+    ['hgt', /^(?:1[5-8][0-9]|19[0-3])cm|(?:59|6[0-9]|7[0-6])in$/],
+    ['hcl', /^#[0-9,a-f]{6}$/],
+    ['ecl', /^amb|blu|brn|gry|grn|hzl|oth$/],
     ['pid', /^\d{9}$/]
 ];
 
@@ -56,7 +57,7 @@ const validateFieldsPart2 = (passports, optionalField = 'cid') => {
                 acc = [...acc, [field[0], Boolean(isMatch)]]
                 return acc;
             } else {
-                acc = [...acc, [field[0], 'not found']]
+                acc = [...acc, [field[0], false]]
                 return acc;
             }
         }, []);
@@ -64,7 +65,7 @@ const validateFieldsPart2 = (passports, optionalField = 'cid') => {
         if (validator.filter(v => v[1] === true).length === requiredFields.length) {
             return passport;
         }
-    }).length;
+    }, []);
 }
 
 const day4Solution = () => {
@@ -73,7 +74,7 @@ const day4Solution = () => {
     const passports = getPassportFields(input);
     return {
         part1: validateFields(passports),
-        part2: validateFieldsPart2(passports)
+        part2: validateFieldsPart2(passports).length
     }
 };
 
