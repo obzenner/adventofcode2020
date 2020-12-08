530f7c447891dde1f73ba1622216127afedea6bf
obzenner <obzenner@Kinjas-MBP.lan>
2020-12-08 02:32:20 +0100

    refactor a bit

--- day7/index.js
+++ day7/index.js
@@ -3,12 +3,83 @@
 const fs = require("fs");
 const path = require('path');
 
-const day7Solution = () => {
-    let bags
-    const data = fs.readFileSync(path.join(__dirname + '/input.txt'), 'utf8');
-    bags = convertData(data)
+function convertData(data) {
+    let entries = data.split("\n")
+
+    return entries.filter(e => e !== "")
+        .map(e => {
+            let [container, content] = e.replace(/bags|\./g, "").split("contain")
+            let [mutation, color] = container.trim().split(" ")
+
+            container = {mutation,color}
+
+            content = content.split(",")
+                .map(c => {
+                    let [quantity, mutation, color] = c.trim().split(" ")
+                    if (quantity !== "no") {
+                        return {
+                            quantity: parseInt(quantity),
+                            mutation,
+                            color
+                        }
+                    }
+                    else 
+                        null
+
+                }).filter(c => c !== undefined)
+
+            return {
+                container: container,
+                content
+            }
+        })
+}
+
+function findBag(bags, mutation, color) {
+    return bags.find(bag => bag.container.mutation === mutation && bag.container.color === color)
+}
+
+
+function recursiveBagFinding(bags, bag) {    
+    bag = findBag(bags, bag.mutation, bag.color)
+
+    if (!bag) return 0
+
+    let contentCount = 0;
     
-    let shinyHoldingBags = findContent("shiny", "gold")
+    //for for every bag in this bag, get the countCount
+    for (let i = 0; i < bag.content.length; i++) {
+        let content = bag.content[i]
+
+        //add the count of this bag's content to the big counter (plus 1 for itself)
+        contentCount += content.quantity * (1 + recursiveBagFinding(bags, content))
+    }
+
+    return contentCount;
+
+}
+
+function partTwo(bags) {
+    let shinyBag = {mutation:"shiny", color:"gold"}
+    return recursiveBagFinding(bags, shinyBag)
+} 
+
+function findContent(bags, mutation, color) {
+    return bags.filter(bag => {
+        let canHold = false
+        bag.content.forEach(c => {
+            if (c.mutation === mutation && c.color === color)
+                canHold = true
+        })
+        return canHold
+    })
+}
+
+const day7Solution = () => {
+    const data = fs.readFileSync(path.join(__dirname + '/input.txt'), 'utf8');
+    const bags = convertData(data)
+    
+    let shinyHoldingBags = findContent(bags, "shiny", "gold")
 
     let allShinyHoldingBags = [...shinyHoldingBags]
     let oldSize = -1
@@ -18,7 +89,7 @@ const day7Solution = () => {
 
         let moreShinyHoldingBags = [] //find the next layer of bags,
         allShinyHoldingBags.forEach(bag => {
-            let moreBags = findContent(bag.container.mutation, bag.container.color)
+            let moreBags = findContent(bags, bag.container.mutation, bag.container.color)
             moreShinyHoldingBags = [...moreShinyHoldingBags, ...moreBags]
         })
 
@@ -32,86 +103,7 @@ const day7Solution = () => {
 
     return {
         part1: allShinyHoldingBags.length,
-        part2: partTwo()
-    }
-    
-    
-    
-    // PART TWO
-    // ============================================================================================
-    
-    function partTwo() {
-        let shinyBag = {mutation:"shiny", color:"gold"}
-        return recursiveBagFinding(shinyBag)
-    }
-    
-    
-    function recursiveBagFinding(bag) {    
-        bag = findBag(bag.mutation, bag.color)
-
-        if (!bag) return 0
-    
-        let contentCount = 0;
-        
-        //for for every bag in this bag, get the countCount
-        for (let i = 0; i < bag.content.length; i++) {
-            let content = bag.content[i]
-    
-            //add the count of this bag's content to the big counter (plus 1 for itself)
-            contentCount += content.quantity * (1 + recursiveBagFinding(content))
-        }
-
-        return contentCount;
-    
-    }
-    
-    
-    function findBag(mutation, color) {
-        return bags.find(bag => bag.container.mutation === mutation && bag.container.color === color)
-    }
-    
-    
-    function findContent(mutation, color) {
-        return bags.filter(bag => {
-            let canHold = false
-            bag.content.forEach(c => {
-                if (c.mutation === mutation && c.color === color)
-                    canHold = true
-            })
-            return canHold
-        })
-    }
-    
-    function convertData(data) {
-        let entries = data.split("\n")
-    
-        return entries.filter(e => e !== "")
-            .map(e => {
-                let [container, content] = e.replace(/bags|\./g, "").split("contain")
-                let [mutation, color] = container.trim().split(" ")
-    
-                container = {mutation,color}
-    
-                content = content.split(",")
-                    .map(c => {
-                        let [quantity, mutation, color] = c.trim().split(" ")
-                        if (quantity !== "no") {
-                            return {
-                                quantity: parseInt(quantity),
-                                mutation,
-                                color
-                            }
-                        }
-                        else 
-                            null
-    
-                    }).filter(c => c !== undefined)
-    
-                return {
-                    container: container,
-                    content
-                }
-            })
+        part2: partTwo(bags)
     }
 }
 
