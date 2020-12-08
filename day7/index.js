d69d75eefb2d3edc0bb8663ae4fa6bb52e9f7e9a
obzenner <obzenner@Kinjas-MBP.lan>
2020-12-08 12:38:59 +0100

    nicer solutions

--- day7/index.js
+++ day7/index.js
@@ -3,107 +3,78 @@
 const fs = require("fs");
 const path = require('path');
 
-function convertData(data) {
-    let entries = data.split("\n")
+const prepareBags = (rawInput) => {
+    return rawInput.split(/\n/).reduce((acc, line) => {
+        const explodedLine = line.split(/ bags |contain |, |\./).filter(i => i !== '');
+        const content = explodedLine.slice(1, explodedLine.length).reduce((acc, bag) => {
+            const explodedBag = bag.split(' ');
+            const content = {
+                quantity: Number(explodedBag[0]),
+                mutation: explodedBag[1],
+                color: explodedBag[2]
+            };
+            return explodedBag[0] !== 'no' ? [...acc, content] : acc;
+        }, []);
 
-    return entries.filter(e => e !== "")
-        .map(e => {
-            let [container, content] = e.replace(/bags|\./g, "").split("contain")
-            let [mutation, color] = container.trim().split(" ")
-
-            container = {mutation,color}
-
-            content = content.split(",")
-                .map(c => {
-                    let [quantity, mutation, color] = c.trim().split(" ")
-                    if (quantity !== "no") {
-                        return {
-                            quantity: parseInt(quantity),
-                            mutation,
-                            color
-                        }
-                    }
-                    else 
-                        null
-
-                }).filter(c => c !== undefined)
-
-            return {
-                container: container,
-                content
-            }
-        })
+        return content.length > 0 ? [...acc, {
+            rule: {
+                mutation: explodedLine[0].split(' ')[0],
+                color: explodedLine[0].split(' ')[1]
+            },
+            content
+        }] : acc
+    }, []);
 }
 
-function findBag(bags, mutation, color) {
-    return bags.find(bag => bag.container.mutation === mutation && bag.container.color === color)
+const findAllBagsContainingInput = (bags, bagToFind = {mutation: 'shiny', color: 'gold'}) => {
+    return bags.filter(bag => {
+        return bag.content.find(item => {
+            return item.mutation === bagToFind.mutation && item.color === bagToFind.color;
+        });
+    });
 }
 
+const getAllBagsContainingInput = (bags, bagsToFind, allBags = []) => {
+   while (bagsToFind && bagsToFind.length > 0) {
+        const newBagsToFind = bagsToFind.reduce((acc, bag) => {
+            return [...acc, ...findAllBagsContainingInput(bags, bag)]
+        }, []).map(b => b.rule);
+        return getAllBagsContainingInput(bags, newBagsToFind, [...allBags, ...newBagsToFind]);
+   }
+   return new Set(allBags);
+}
 
-function recursiveBagFinding(bags, bag) {    
-    bag = findBag(bags, bag.mutation, bag.color)
+const findCurrentBag = (bags, bagToFind = {mutation: 'shiny', color: 'gold'}) => {
+    return bags.find(bag => {
+        return bag.rule.mutation === bagToFind.mutation && bag.rule.color === bagToFind.color;
+    });
+}
 
-    if (!bag) return 0
+const getAllBagsInsideInput = (bags, bagToFind, sum = 0) => {
+    const currentBag = findCurrentBag(bags, bagToFind);
 
-    let contentCount = 0;
-    
-    //for for every bag in this bag, get the countCount
-    for (let i = 0; i < bag.content.length; i++) {
-        let content = bag.content[i]
+    if (!currentBag) return 0;
 
-        //add the count of this bag's content to the big counter (plus 1 for itself)
-        contentCount += content.quantity * (1 + recursiveBagFinding(bags, content))
+    for (let i = 0; i < currentBag.content.length; i++) {
+        const childContent = currentBag.content[i];
+
+        sum += childContent.quantity * (1 + getAllBagsInsideInput(bags, childContent));
     }
 
-    return contentCount;
-
-}
-
-function partTwo(bags) {
-    let shinyBag = {mutation:"shiny", color:"gold"}
-    return recursiveBagFinding(bags, shinyBag)
-} 
-
-function findContent(bags, mutation, color) {
-    return bags.filter(bag => {
-        let canHold = false
-        bag.content.forEach(c => {
-            if (c.mutation === mutation && c.color === color)
-                canHold = true
-        })
-        return canHold
-    })
+    return sum;
 }
 
 const day7Solution = () => {
-    const data = fs.readFileSync(path.join(__dirname + '/input.txt'), 'utf8');
-    const bags = convertData(data)
-    
-    let shinyHoldingBags = findContent(bags, "shiny", "gold")
+    const rawInput = fs.readFileSync(path.join(__dirname + '/input.txt'), 'utf8');
+    const bags = prepareBags(rawInput);
+    const INPUT_BAG = [{mutation: 'shiny', color: 'gold'}];
 
-    let allShinyHoldingBags = [...shinyHoldingBags]
-    let oldSize = -1
-    let newSize = -2
-    do {
-        oldSize = allShinyHoldingBags.length
-
-        let moreShinyHoldingBags = [] //find the next layer of bags,
-        allShinyHoldingBags.forEach(bag => {
-            let moreBags = findContent(bags, bag.container.mutation, bag.container.color)
-            moreShinyHoldingBags = [...moreShinyHoldingBags, ...moreBags]
-        })
-
-        //remove duplicates (there shouldn't be any dupes if I was any good from the first place)
-        allShinyHoldingBags = Array.from(new Set([...allShinyHoldingBags, ...moreShinyHoldingBags]))
-
-        newSize = allShinyHoldingBags.length
-
-        //if no more new bags are found, head out
-    } while (oldSize != newSize)
+    const allBagsThatContainInput = getAllBagsContainingInput(bags, INPUT_BAG);
+    const allBagsInsideTheInput = getAllBagsInsideInput(bags, INPUT_BAG[0])
+    console.log(allBagsInsideTheInput)
 
     return {
-        part1: allShinyHoldingBags.length,
-        part2: partTwo(bags)
+        part1: allBagsThatContainInput.size
     }
 }
 
