2f67aab7ed8476b03d9775f83efe2ae8b28dc10c
obzenner <obzenner@Kinjas-MBP.lan>
2020-12-08 02:12:57 +0100

    day 7

---
+++ day7/index.js
@@ -0,0 +1,120 @@
+'use strict';
+
+const fs = require("fs");
+const path = require('path');
+
+const day7Solution = () => {
+    let bags
+    const data = fs.readFileSync(path.join(__dirname + '/input.txt'), 'utf8');
+    bags = convertData(data)
+    
+    let shinyHoldingBags = findContent("shiny", "gold")
+
+    let allShinyHoldingBags = [...shinyHoldingBags]
+    let oldSize = -1
+    let newSize = -2
+    do {
+        oldSize = allShinyHoldingBags.length
+
+        let moreShinyHoldingBags = [] //find the next layer of bags,
+        allShinyHoldingBags.forEach(bag => {
+            let moreBags = findContent(bag.container.mutation, bag.container.color)
+            moreShinyHoldingBags = [...moreShinyHoldingBags, ...moreBags]
+        })
+
+        //remove duplicates (there shouldn't be any dupes if I was any good from the first place)
+        allShinyHoldingBags = Array.from(new Set([...allShinyHoldingBags, ...moreShinyHoldingBags]))
+
+        newSize = allShinyHoldingBags.length
+
+        //if no more new bags are found, head out
+    } while (oldSize != newSize)
+
+    return {
+        part1: allShinyHoldingBags.length,
+        part2: partTwo()
+    }
+    
+    
+    
+    // PART TWO
+    // ============================================================================================
+    
+    function partTwo() {
+        let shinyBag = {mutation:"shiny", color:"gold"}
+        return recursiveBagFinding(shinyBag)
+    }
+    
+    
+    function recursiveBagFinding(bag) {    
+        bag = findBag(bag.mutation, bag.color)
+        
+        if (!bag) return 0
+    
+        let contentCount = 0;
+        
+        //for for every bag in this bag, get the countCount
+        for (let i = 0; i < bag.content.length; i++) {
+            let content = bag.content[i]
+            
+            //add the count of this bag's content to the big counter (plus 1 for itself)
+            contentCount += content.quantity * (1 + recursiveBagFinding(content))
+        }
+        
+        return contentCount 
+    
+    }
+    
+    
+    function findBag(mutation, color) {
+        return bags.find(bag => bag.container.mutation === mutation && bag.container.color === color)
+    }
+    
+    
+    function findContent(mutation, color) {
+        return bags.filter(bag => {
+            let canHold = false
+            bag.content.forEach(c => {
+                if (c.mutation === mutation && c.color === color)
+                    canHold = true
+            })
+            return canHold
+        })
+    }
+    
+    function convertData(data) {
+        let entries = data.split("\n")
+    
+        return entries.filter(e => e !== "")
+            .map(e => {
+                let [container, content] = e.replace(/bags|\./g, "").split("contain")
+                let [mutation, color] = container.trim().split(" ")
+    
+                container = {mutation,color}
+    
+                content = content.split(",")
+                    .map(c => {
+                        let [quantity, mutation, color] = c.trim().split(" ")
+                        if (quantity !== "no") {
+                            return {
+                                quantity: parseInt(quantity),
+                                mutation,
+                                color
+                            }
+                        }
+                        else 
+                            null
+    
+                    }).filter(c => c !== undefined)
+    
+                return {
+                    container: container,
+                    content
+                }
+            })
+    }
+}
+
+module.exports = {
+    day7Solution
+}
--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const { day3Solution } = require('./day3/index');
 const { day4Solution } = require('./day4/index');
 const { day5Solution } = require('./day5/index');
 const { day6Solution } = require('./day6/index');
+const { day7Solution } = require('./day7/index');
+
 const day1Result = day1Solution();
 const day2Result = day2Solution();
 const day3Result = day3Solution();
 const day4Result = day4Solution();
 const day5Result = day5Solution();
 const day6Result = day6Solution();
+const day7Result = day7Solution();
 
-console.log(day6Result)
+console.log(day7Result)
