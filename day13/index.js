f688f957f88db3c9191154d85a2a3049802ad8a6
obzenner <obzenner@Kinjas-MBP.lan>
2020-12-14 02:24:43 +0100

    chinese theorem

--- day13/index.js
+++ day13/index.js
@@ -3,6 +3,31 @@
 const fs = require("fs");
 const path = require('path');
 
+const modInverse = (a, m) => {
+    let g = gcd(a, m);
+
+    if (g != 1n) {
+        console.log("No Inverse");
+    } else {
+        return power(a, m - 2n, m)
+    }
+}
+
+const power = (x, y, m) => {
+    if (y === 0n) return 1n;
+
+    let p = power(x, y / 2n, m) % m;
+    p = (p * p) % m;
+
+    if (y % 2n === 0n) return p;
+    else return ((x * p) % m);
+}
+
+const gcd = (a, b) => {
+    if (a === 0n) return b;
+    return gcd(b % a, a)
+}
+
 const calcTimestampDiff = (testAgainst = 939, timestamp) => {
     const closestTimestamp = timestamp * Math.ceil(testAgainst / timestamp);
 
@@ -39,9 +64,24 @@ function findTimeStampBayla(busRecords) {
                 timestamp += mult;
             }
         }
-    
-        return {timestamp, mult};
-    }, {timestamp: 0, mult: 1})
+
+        return { timestamp, mult };
+    }, { timestamp: 0, mult: 1 })
+}
+
+//chinese remainder theorem
+const chineseRemainderSolution = (pairs) => {
+    const N = pairs.reduce((acc, p) => {
+        return acc * p[0];
+    }, 1n);
+
+    const Ni = pairs.map(pair => N / pair[0]);
+    const b = pairs.map((pair, i) => pair[1] === 0n ? 0n : pair[0] - pair[1]);
+    const x = pairs.map((item, i) => modInverse(Ni[i], item[0]))
+    const bnx = Ni.map((item, i) => item * b[i] * x[i])
+
+    const sum = bnx.reduce((acc, cur) => acc + cur)
+    return sum - (sum / N) * N
 }
 
 const day13Solution = () => {
@@ -59,12 +99,18 @@ const day13Solution = () => {
         }, [])
     };
 
-    const minDiff = getClosestDiff({earliestTimestamp, timestamps});
+    const minDiff = getClosestDiff({ earliestTimestamp, timestamps });
     const { timestamp } = findTimeStampBayla(timestamps)
+    const chineseTheorem = chineseRemainderSolution(rawInput[1].split(",").reduce((acc, v, i) => {
+        if (v !== 'x') {
+            acc = [...acc, [BigInt(v), BigInt(i)]]
+        }
+        return acc;
+    }, []));
 
     return {
         part1: minDiff.timestamp * minDiff.minutes,
-        part2: timestamp
+        part2: chineseTheorem
     }
 }
 
