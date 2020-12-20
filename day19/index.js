13896ab6c8b0c3ebe965772a0c225c6910c5546f
obzenner <obzenner@Kinjas-MBP.lan>
2020-12-20 15:49:51 +0100

    add init day 19 (again, f***)

--- day19/index.js
+++ day19/index.js
@@ -5,13 +5,39 @@ const path = require('path');
 
 
 const day19Solution = () => {
-    const raw = fs.readFileSync(path.join(__dirname + '/input.txt'), 'utf8').split(/\n/);
+    const input = fs.readFileSync(path.join(__dirname + '/input.txt'), 'utf8')
+        .split(/\n/)
+        .reduce((acc, line, i, arr) => {
+            if (acc.status === 'rules' && line !== '') {
+                const exploded = line.split(' ');
+                acc.rules = [...acc.rules, {
+                    ruleId: Number(exploded[0][0]),
+                    body: exploded.slice(1).map(v => {
+                        if (['a', 'b', '|'].includes(v)) return v;
+                        else return Number(v);
+                    })
+                }];
+            } else {
+                acc.messages = [...acc.messages, line];
+            }
 
+            if (line === '') {
+                acc.status = 'messages';
+                return acc;
+            }
 
+            if (i === arr.length - 1) {
+                delete acc.status
+            }
+
+            return acc;
+        }, { rules: [], messages: [], status: 'rules'})
+
+    console.log(input)
 
     return {
-        part1: resPt1,
-        part2: resPt2
+        part1: null,
+        part2: null
     }
 }
 
